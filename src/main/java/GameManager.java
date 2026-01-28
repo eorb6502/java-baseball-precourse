fd631ebf687615469b7ef70c578c714fa25aa1d8
Cha Dae Gyu <eorb6502@kaist.ac.kr>
2026-01-29 00:32:55 +0900

    GameManager refactoring

--- src/main/java/GameManager.java
+++ src/main/java/GameManager.java
@@ -1,3 +1,6 @@
+import java.util.Map;
+import java.util.HashMap;
+
 public class GameManager {
     private TurnPhase _turnPhase;
     private NumberGenerator _numberGenerator;
@@ -5,50 +8,51 @@ public class GameManager {
     private JudgeManager _judgeManager;
     private String _generatedNumber;
     private String _userNumber;
+    private Map<TurnPhase, Runnable> _phaseHandler;
     public GameManager(){
         _turnPhase = TurnPhase.idle;
         _numberGenerator = new NumberGenerator();
         _inputManager = new InputManager();
         _judgeManager = new JudgeManager();
+
+        _phaseHandler = new HashMap<>();
+        _phaseHandler.put(TurnPhase.idle, this::handleIdlePhase);
+        _phaseHandler.put(TurnPhase.generateNumber, this::handleGenerateNumberPhase);
+        _phaseHandler.put(TurnPhase.inputNumber, this::handleInputNumberPhase);
+        _phaseHandler.put(TurnPhase.judgeResult, this::handleJudgeResultPhase);
     }
-    private void idlePhasePlay() {
-        if (_turnPhase != TurnPhase.idle) return;
+    private void handleIdlePhase() {
         boolean idleFlag = _inputManager.IdleInput();
-        if (idleFlag){
-            _turnPhase = TurnPhase.generateNumber;
-            _generatedNumber = "";
+        
+        _turnPhase = idleFlag ? TurnPhase.generateNumber : TurnPhase.quit;
+        _generatedNumber = idleFlag ? "" : _generatedNumber;
+    }
+
+    private void handleGenerateNumberPhase() {
+        _generatedNumber = _numberGenerator.GenerateNumber();
+        _turnPhase = TurnPhase.inputNumber;
+    }
+
+    private void handleInputNumberPhase() {
+        _userNumber = _inputManager.PredictionInput();
+        System.out.println(_userNumber);
+        _turnPhase = TurnPhase.judgeResult;
+    }
+    
+    private void handleJudgeResultPhase() {
+        boolean result = _judgeManager.JudgeResult(_generatedNumber, _userNumber);
+        if (!result) {
+            _turnPhase = TurnPhase.inputNumber;
             return;
         }
-        _turnPhase = TurnPhase.quit;
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 끝");
+        _turnPhase = TurnPhase.idle;
     }
 
     public void PlayGame(){
-        while(true){
-            if (_turnPhase == TurnPhase.quit) break;
-            if (_turnPhase == TurnPhase.idle) {
-                idlePhasePlay();
-                continue;
-            }
-            if (_turnPhase == TurnPhase.generateNumber) {
-                _generatedNumber = _numberGenerator.GenerateNumber();
-                _turnPhase = TurnPhase.inputNumber;
-                continue;
-            }
-            if (_turnPhase == TurnPhase.inputNumber) {
-                _userNumber = _inputManager.PredictionInput();
-                System.out.println(_userNumber);
-                _turnPhase = TurnPhase.judgeResult;
-                continue;
-            }
-            if (_turnPhase == TurnPhase.judgeResult){
-                boolean result = _judgeManager.JudgeResult(_generatedNumber, _userNumber);
-                if (result) {
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 끝");
-                    _turnPhase = TurnPhase.idle;
-                    continue;
-                }
-                _turnPhase = TurnPhase.inputNumber;
-            }
+        while(_turnPhase != TurnPhase.quit){
+            Runnable handler = _phaseHandler.get(_turnPhase);
+            handler.run();
         }
     }
 }
