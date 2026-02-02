313e4c24ddbcf5d0e48514fed54b25a5ab3006e8
theo.cha <theo.cha@kakaocorp.com>
2026-02-02 17:31:43 +0900

    magic number delete 2

--- src/main/java/NumberGenerator.java
+++ src/main/java/NumberGenerator.java
@@ -3,12 +3,13 @@ import java.util.Map;
 import java.util.Random;
 
 public class NumberGenerator {
+    private static final int BASEBALL_SIZE = 3;
     public NumberGenerator() {}
     public String GenerateNumber() {
         String result = "";
         Map<String, String> map = new HashMap<>();
         Random random = new Random();
-        while(result.length() < 3) {
+        while(result.length() < BASEBALL_SIZE) {
             String randomNumber =random.nextInt(1, 9) + "";
             if (map.get(randomNumber) != null) continue;
             map.put(randomNumber, "");
