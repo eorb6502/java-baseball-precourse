a118617820cfef5cd00fff1ed99d7646c31cbe94
theo.cha <theo.cha@kakaocorp.com>
2026-02-02 17:30:49 +0900

    magic number delete

--- src/main/java/JudgeManager.java
+++ src/main/java/JudgeManager.java
@@ -2,15 +2,17 @@ import java.util.Map;
 import java.util.HashMap;
 public class JudgeManager {
     public JudgeManager() {}
+    private static final int BASEBALL_SIZE = 3;
+    private static final int PLAYER_CNT = 2;
     public JudgeResult JudgeResult(String number, String user){
         Map<Character, String> map = new HashMap<>();
-        for (int i=0; i<3; i++){
+        for (int i=0; i<BASEBALL_SIZE; i++){
             map.put(number.charAt(i), "");
             map.put(user.charAt(i), "");
         }
-        int ball = 6 - map.size();
+        int ball = BASEBALL_SIZE * PLAYER_CNT - map.size();
         int strike = 0;
-        for (int i=0; i<3; i++){
+        for (int i=0; i<BASEBALL_SIZE; i++){
             if (number.charAt(i) == user.charAt(i)) strike ++;
         }
         ball -= strike;
